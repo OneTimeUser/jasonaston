fcb48a6c9423007cb368f2b119763706292beb1f
Jason Aston <jasonaston@infoadmins-MacBook-Pro.local>
2023-06-22 11:27:11 -0400

    Been a while...

---
+++ introweb-old/site/accounts/jason.php
@@ -0,0 +1,16 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: jason
+email: aston@cs.nyu.edu
+password: >
+  $2a$10$VJSJL.xBZOSsCKpUHJwN9eJ39gRuKtUI2H3NuEgE38z/GuvnhAaO.
+language: en
+role: admin
+history:
+  - week-15-final-review
+  - week-13-2-jquery
+  - week-14-2-last-necessary-steps
+  - week-14-1-javascript
+  - assignments
+firstname: ""
+lastname: ""
